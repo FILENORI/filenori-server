b7054bd3762922e6b7078aecb9e5e4f871c4b301
HO22 <flsghl@naver.com>
2024-12-17 18:54:21 +0900

    refactoring

--- src/RequestHandler.hpp
+++ src/RequestHandler.hpp
@@ -1,42 +1,87 @@
 #ifndef REQUEST_HANDLER_HPP
 #define REQUEST_HANDLER_HPP
 
-#include "PeerQueueManager.hpp"
-#include "FileManager.hpp"
 #include <boost/asio.hpp>
-#include <nlohmann/json.hpp>
+#include <fstream>
 #include <iostream>
+#include <nlohmann/json.hpp>
+
+#include "FileManager.hpp"
+#include "PeerQueueManager.hpp"
 
 using boost::asio::ip::tcp;
 using json = nlohmann::json;
 
 class RequestHandler {
-private:
-    PeerQueueManager& peerQueueManager;
-    FileManager& fileManager;
+ private:
+  PeerQueueManager& peerQueueManager;
+  FileManager& fileManager;
 
-public:
-    RequestHandler(PeerQueueManager& p, FileManager& f)
-        : peerQueueManager(p), fileManager(f) {}
+ public:
+  RequestHandler(PeerQueueManager& p, FileManager& f)
+      : peerQueueManager(p), fileManager(f) {}
 
-    void handleHeartbeat(tcp::socket& socket, const json& request) {
-        peerQueueManager.updatePeerFiles(request["peer_ip"], request["files"]);
-        json response = {{"status", "success"}, {"message", "Heartbeat received"}};
-        boost::asio::write(socket, boost::asio::buffer(response.dump() + "<END>"));
+  void handleHeartbeat(tcp::socket& socket, const json& request) {
+    peerQueueManager.updatePeerFiles(request["peer_ip"], request["files"]);
+    json response = {{"status", "success"}, {"message", "Heartbeat received"}};
+    boost::asio::write(socket, boost::asio::buffer(response.dump() + "<END>"));
+  }
+
+  void handleUpload(tcp::socket& socket, const json& request) {
+    std::string fileName = request["file_name"];
+    size_t fileSize = request["file_size"];
+    std::string fileId = fileManager.saveFileMetadata(fileName, fileSize);
+    json response = {{"status", "success"},
+                     {"message", "File uploaded successfully"},
+                     {"uuid", fileId}};
+    boost::asio::write(socket, boost::asio::buffer(response.dump() + "<END>"));
+  }
+
+  void handleListFiles(tcp::socket& socket) {
+    json response = {{"status", "success"}, {"files", fileManager.listFiles()}};
+    boost::asio::write(socket, boost::asio::buffer(response.dump() + "<END>"));
+  }
+
+  void handleFindFile(tcp::socket& socket, const json& request) {
+    std::string fileId = request["file_id"];
+    std::vector<std::string> peers = peerQueueManager.findPeersByFileId(fileId);
+    json response = {{"status", peers.empty() ? "error" : "success"},
+                     {"peers", peers},
+                     {"message", peers.empty() ? "File not found on any peer"
+                                               : "File found"}};
+    boost::asio::write(socket, boost::asio::buffer(response.dump() + "<END>"));
+  }
+
+  void handleDownload(tcp::socket& socket, const json& request) {
+    std::string fileId = request["file_id"];
+    std::string fileName = fileManager.getFileName(fileId);
+
+    std::ifstream file(fileName, std::ios::binary);
+    if (!file.is_open()) {
+      json response = {{"status", "error"}, {"message", "File not found"}};
+      boost::asio::write(socket,
+                         boost::asio::buffer(response.dump() + "<END>"));
+      return;
     }
 
-    void handleUpload(tcp::socket& socket, const json& request) {
-        std::string fileName = request["file_name"];
-        size_t fileSize = request["file_size"];
-        std::string fileId = fileManager.saveFileMetadata(fileName, fileSize);
-        json response = {{"status", "success"}, {"message", "File uploaded successfully"}, {"uuid", fileId}};
-        boost::asio::write(socket, boost::asio::buffer(response.dump() + "<END>"));
+    char buffer[1024];
+    while (file.good()) {
+      file.read(buffer, sizeof(buffer));
+      std::streamsize bytesRead = file.gcount();
+      boost::asio::write(socket, boost::asio::buffer(buffer, bytesRead));
     }
+    file.close();
 
-    void handleListPeers(tcp::socket& socket) {
-        json response = {{"status", "success"}, {"peers", peerQueueManager.listPeers()}};
-        boost::asio::write(socket, boost::asio::buffer(response.dump() + "<END>"));
-    }
+    json response = {{"status", "success"},
+                     {"message", "File downloaded successfully"}};
+    boost::asio::write(socket, boost::asio::buffer(response.dump() + "<END>"));
+  }
+
+  void handleListPeers(tcp::socket& socket) {
+    json response = {{"status", "success"},
+                     {"peers", peerQueueManager.listPeers()}};
+    boost::asio::write(socket, boost::asio::buffer(response.dump() + "<END>"));
+  }
 };
 
-#endif // REQUEST_HANDLER_HPP
+#endif  // REQUEST_HANDLER_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,76 @@
-#include "PeerQueueManager.hpp"
-#include "FileManager.hpp"
-#include "RequestHandler.hpp"
 #include <boost/asio.hpp>
-#include <nlohmann/json.hpp>
 #include <iostream>
+#include <nlohmann/json.hpp>
+
+#include "FileManager.hpp"
+#include "PeerQueueManager.hpp"
+#include "RequestHandler.hpp"
 
 using boost::asio::ip::tcp;
 using json = nlohmann::json;
 
 class MainServer {
-private:
-    boost::asio::io_context ioContext;
-    tcp::acceptor acceptor;
-    PeerQueueManager peerQueueManager;
-    FileManager fileManager;
-    RequestHandler requestHandler;
+ private:
+  boost::asio::io_context ioContext;
+  tcp::acceptor acceptor;
+  PeerQueueManager peerQueueManager;
+  FileManager fileManager;
+  RequestHandler requestHandler;
 
-public:
-    MainServer(int port)
-        : acceptor(ioContext, tcp::endpoint(tcp::v4(), port)),
-          requestHandler(peerQueueManager, fileManager) {}
+ public:
+  MainServer(int port)
+      : acceptor(ioContext, tcp::endpoint(tcp::v4(), port)),
+        requestHandler(peerQueueManager, fileManager) {}
 
-    void start() {
-        std::cout << "TEST Server running on port " << acceptor.local_endpoint().port() << "..." << std::endl;
-        while (true) {
-            tcp::socket socket(ioContext);
-            acceptor.accept(socket);
-            std::cout << "Client connected!" << std::endl;
-            handleRequest(socket);
-        }
+  void start() {
+    std::cout << "Server running on port " << acceptor.local_endpoint().port()
+              << "..." << std::endl;
+    while (true) {
+      tcp::socket socket(ioContext);
+      acceptor.accept(socket);
+      std::cout << "Client connected!" << std::endl;
+      handleRequest(socket);
     }
+  }
 
-    void handleRequest(tcp::socket& socket) {
-        char buffer[2048];
-        boost::system::error_code error;
+  void handleRequest(tcp::socket& socket) {
+    char buffer[2048];
+    boost::system::error_code error;
 
-        while (true) {
-            size_t bytesRead = socket.read_some(boost::asio::buffer(buffer), error);
-            if (error == boost::asio::error::eof) break;
-            std::string requestStr(buffer, bytesRead);
-            json request = json::parse(requestStr);
-            std::string action = request["action"];
+    while (true) {
+      size_t bytesRead = socket.read_some(boost::asio::buffer(buffer), error);
+      if (error == boost::asio::error::eof) break;
+      std::string requestStr(buffer, bytesRead);
+      json request = json::parse(requestStr);
+      std::string action = request["action"];
 
-            if (action == "heartbeat") requestHandler.handleHeartbeat(socket, request);
-            else if (action == "upload") requestHandler.handleUpload(socket, request);
-            else if (action == "list_peers") requestHandler.handleListPeers(socket);
-            else {
-                json response = {{"status", "error"}, {"message", "Unknown action"}};
-                boost::asio::write(socket, boost::asio::buffer(response.dump() + "<END>"));
-            }
-        }
+      if (action == "heartbeat")
+        requestHandler.handleHeartbeat(socket, request);
+      else if (action == "upload")
+        requestHandler.handleUpload(socket, request);
+      else if (action == "list_files")
+        requestHandler.handleListFiles(socket);
+      else if (action == "find_file")
+        requestHandler.handleFindFile(socket, request);
+      else if (action == "download")
+        requestHandler.handleDownload(socket, request);
+      else if (action == "list_peers")
+        requestHandler.handleListPeers(socket);
+      else {
+        json response = {{"status", "error"}, {"message", "Unknown action"}};
+        boost::asio::write(socket,
+                           boost::asio::buffer(response.dump() + "<END>"));
+      }
     }
+  }
 };
 
 int main() {
-    try {
-        MainServer server(12345);
-        server.start();
-    } catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-    }
-    return 0;
+  try {
+    MainServer server(12345);
+    server.start();
+  } catch (const std::exception& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+  }
+  return 0;
 }
